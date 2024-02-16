ea6e3623c17e9f2a1e292cf2032de99daaf8e73a
Bruce Borrett <oceativa@gmail.com>
2024-02-16 17:11:38 +0200

    Create user roles and admin user if do not exist.

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,27 @@
 import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
+import { Roles } from 'meteor/alanning:roles';
+import { Accounts } from 'meteor/accounts-base';
 
-async function insertLink({ title, url }) {
+async function insertLink({ title, url }: { title: string; url: string }) {
   await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
+}
+
+// Create user roles if they don't exist
+if (Roles.getAllRoles().count() === 0) {
+  Meteor.settings.userRoles.forEach((role: string) => {
+    Roles.createRole(role);
+  });
+}
+
+// Create admin user if one doesn't exist
+if (Meteor.users.find().count() === 0) {
+  const user = Accounts.createUser({
+    email: Meteor.settings.adminEmail,
+    password: Meteor.settings.adminPassword,
+  });
+
+  Roles.addUsersToRoles(user, 'admin');
 }
 
 Meteor.startup(async () => {
